541a05fe334cec06d5a9604e15f783850b1b5612
dj-viking <anders.swedishviking@gmail.com>
2023-10-29 13:53:29 -0400

    getting started

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -1,2 +1,29 @@
 "use strict";
-console.log("hello world");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const fileText = fs_1.default.readFileSync("input.txt", { encoding: "utf-8" });
+var Direction;
+(function (Direction) {
+    Direction["north"] = "north";
+    Direction["south"] = "south";
+    Direction["east"] = "east";
+    Direction["west"] = "west";
+})(Direction || (Direction = {}));
+class NorthPole {
+    constructor() {
+        // present delivered at start
+        this.presentsDelivered = 1;
+        this.previousDirection = null;
+        this.housepath = fileText;
+        this.pathArray = [];
+        this.pathArray = this.housepath.split("");
+    }
+    main() {
+        console.log("hello north pole", this.pathArray);
+    }
+}
+const northpole = new NorthPole();
+northpole.main();
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,1 +1,28 @@
-console.log("hello world");
+import fs from "fs";
+const fileText = fs.readFileSync("input.txt", { encoding: "utf-8" });
+
+enum Direction {
+    north = "north",
+    south = "south",
+    east = "east",
+    west = "west",
+}
+class NorthPole {
+    // present delivered at start
+    public presentsDelivered = 1;
+    public previousDirection: null | Direction = null;
+    public housepath: string = fileText;
+    public pathArray: string[] = [];
+
+    constructor() {
+        this.pathArray = this.housepath.split("");
+    }
+
+    public main() {
+        console.log("hello north pole", this.pathArray);
+    }
+}
+
+const northpole = new NorthPole();
+
+northpole.main();
