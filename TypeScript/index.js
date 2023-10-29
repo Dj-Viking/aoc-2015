031f817c9c75a3f61dbd2640c7c479a1452fe782
dj-viking <anders.swedishviking@gmail.com>
2023-10-29 17:57:15 -0400

    solve part 2 day 3

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -1,7 +1,7 @@
 "use strict";
 const { day3Input: fileText_ } = require("./lib");
 let fileText = fileText_;
-let example = "^>v<";
+let example = "^v";
 var Direction;
 (function (Direction) {
     Direction["^"] = "north";
@@ -28,8 +28,10 @@ class Point {
         return `${this.x},${this.y}`;
     }
 }
+class LocationMap extends Map {
+}
 (function main() {
-    const locationMap = new Map();
+    const locationMap = new LocationMap();
     const point = new Point(0, 0);
     locationMap.set(point.toString(), { visited: point.visited });
     let x = 0;
@@ -90,4 +92,130 @@ class Point {
         }
     }
     console.log("ANSWER PART 1", atleastOnce);
+})();
+(function main2() {
+    let answer2 = 0;
+    let x_santa = 0;
+    let y_santa = 0;
+    let x_robo = 0;
+    let y_robo = 0;
+    const santaMap = new LocationMap();
+    const roboMap = new LocationMap();
+    const point = new Point(0, 0);
+    santaMap.set(point.toString(), { visited: point.visited });
+    roboMap.set(point.toString(), { visited: point.visited });
+    // santa goes first then robo santa
+    let i = 0;
+    for (const direction of fileText.split("")) {
+        // for (const direction of example.split("") as DirectionVal[]) {
+        const turn = i % 2; // 0 for santa, 1 for robo
+        if (turn === 0) {
+            // santa turn
+            if (direction === DirectionName.east) {
+                x_santa += 1;
+                y_santa += 0;
+                const pt = new Point(x_santa, y_santa);
+                if (!santaMap.has(pt.toString())) {
+                    santaMap.set(pt.toString(), { visited: pt.visited });
+                }
+                else {
+                    santaMap.get(pt.toString()).visited++;
+                }
+            }
+            else if (direction === DirectionName.west) {
+                x_santa -= 1;
+                y_santa += 0;
+                const pt = new Point(x_santa, y_santa);
+                if (!santaMap.has(pt.toString())) {
+                    santaMap.set(pt.toString(), { visited: pt.visited });
+                }
+                else {
+                    santaMap.get(pt.toString()).visited++;
+                }
+            }
+            else if (direction === DirectionName.north) {
+                x_santa -= 0;
+                y_santa += 1;
+                const pt = new Point(x_santa, y_santa);
+                if (!santaMap.has(pt.toString())) {
+                    santaMap.set(pt.toString(), { visited: pt.visited });
+                }
+                else {
+                    santaMap.get(pt.toString()).visited++;
+                }
+            }
+            else if (direction === DirectionName.south) {
+                x_santa -= 0;
+                y_santa -= 1;
+                const pt = new Point(x_santa, y_santa);
+                if (!santaMap.has(pt.toString())) {
+                    santaMap.set(pt.toString(), { visited: pt.visited });
+                }
+                else {
+                    santaMap.get(pt.toString()).visited++;
+                }
+            }
+        }
+        else {
+            // robo turn
+            if (direction === DirectionName.east) {
+                x_robo += 1;
+                y_robo += 0;
+                const pt = new Point(x_robo, y_robo);
+                if (!roboMap.has(pt.toString())) {
+                    roboMap.set(pt.toString(), { visited: pt.visited });
+                }
+                else {
+                    roboMap.get(pt.toString()).visited++;
+                }
+            }
+            else if (direction === DirectionName.west) {
+                x_robo -= 1;
+                y_robo += 0;
+                const pt = new Point(x_robo, y_robo);
+                if (!roboMap.has(pt.toString())) {
+                    roboMap.set(pt.toString(), { visited: pt.visited });
+                }
+                else {
+                    roboMap.get(pt.toString()).visited++;
+                }
+            }
+            else if (direction === DirectionName.north) {
+                x_robo -= 0;
+                y_robo += 1;
+                const pt = new Point(x_robo, y_robo);
+                if (!roboMap.has(pt.toString())) {
+                    roboMap.set(pt.toString(), { visited: pt.visited });
+                }
+                else {
+                    roboMap.get(pt.toString()).visited++;
+                }
+            }
+            else if (direction === DirectionName.south) {
+                x_robo -= 0;
+                y_robo -= 1;
+                const pt = new Point(x_robo, y_robo);
+                if (!roboMap.has(pt.toString())) {
+                    roboMap.set(pt.toString(), { visited: pt.visited });
+                }
+                else {
+                    roboMap.get(pt.toString()).visited++;
+                }
+            }
+        }
+        i++;
+    }
+    const santaEntries = santaMap.entries();
+    const roboEntries = roboMap.entries();
+    const visited = new Set();
+    for (let i = 0; i < santaMap.size; i++) {
+        const santaEntry = santaEntries.next().value;
+        visited.add(santaEntry[0]);
+    }
+    for (let i = 0; i < roboMap.size; i++) {
+        const roboEntry = roboEntries.next().value;
+        visited.add(roboEntry[0]);
+    }
+    answer2 = visited.size;
+    console.log("ANSWER PART 2", answer2);
 })();
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,7 +1,7 @@
 const { day3Input: fileText_ } = require("./lib");
 
 let fileText: string = fileText_;
-let example: string = "^>v<";
+let example: string = "^v";
 enum Direction {
     "^" = "north",
     "v" = "south",
@@ -32,11 +32,13 @@ class Point {
     }
 }
 
+class LocationMap extends Map<
+    ReturnType<(typeof Point)["toString"]>,
+    { visited: number }
+> {}
+
 (function main() {
-    const locationMap = new Map<
-        ReturnType<(typeof Point)["toString"]>,
-        { visited: number }
-    >();
+    const locationMap = new LocationMap();
     const point = new Point(0, 0);
     locationMap.set(point.toString(), { visited: point.visited });
     let x = 0;
@@ -93,4 +95,122 @@ class Point {
     }
 
     console.log("ANSWER PART 1", atleastOnce);
+})();
+
+(function main2() {
+    let answer2 = 0;
+    let x_santa = 0;
+    let y_santa = 0;
+    let x_robo = 0;
+    let y_robo = 0;
+    const santaMap = new LocationMap();
+    const roboMap = new LocationMap();
+
+    const point = new Point(0, 0);
+    santaMap.set(point.toString(), { visited: point.visited });
+    roboMap.set(point.toString(), { visited: point.visited });
+
+    // santa goes first then robo santa
+    let i = 0;
+    for (const direction of fileText.split("") as DirectionVal[]) {
+        // for (const direction of example.split("") as DirectionVal[]) {
+        const turn = i % 2; // 0 for santa, 1 for robo
+        if (turn === 0) {
+            // santa turn
+            if (direction === DirectionName.east) {
+                x_santa += 1;
+                y_santa += 0;
+                const pt = new Point(x_santa, y_santa);
+                if (!santaMap.has(pt.toString())) {
+                    santaMap.set(pt.toString(), { visited: pt.visited });
+                } else {
+                    santaMap.get(pt.toString())!.visited++;
+                }
+            } else if (direction === DirectionName.west) {
+                x_santa -= 1;
+                y_santa += 0;
+                const pt = new Point(x_santa, y_santa);
+                if (!santaMap.has(pt.toString())) {
+                    santaMap.set(pt.toString(), { visited: pt.visited });
+                } else {
+                    santaMap.get(pt.toString())!.visited++;
+                }
+            } else if (direction === DirectionName.north) {
+                x_santa -= 0;
+                y_santa += 1;
+                const pt = new Point(x_santa, y_santa);
+                if (!santaMap.has(pt.toString())) {
+                    santaMap.set(pt.toString(), { visited: pt.visited });
+                } else {
+                    santaMap.get(pt.toString())!.visited++;
+                }
+            } else if (direction === DirectionName.south) {
+                x_santa -= 0;
+                y_santa -= 1;
+                const pt = new Point(x_santa, y_santa);
+                if (!santaMap.has(pt.toString())) {
+                    santaMap.set(pt.toString(), { visited: pt.visited });
+                } else {
+                    santaMap.get(pt.toString())!.visited++;
+                }
+            }
+        } else {
+            // robo turn
+            if (direction === DirectionName.east) {
+                x_robo += 1;
+                y_robo += 0;
+                const pt = new Point(x_robo, y_robo);
+                if (!roboMap.has(pt.toString())) {
+                    roboMap.set(pt.toString(), { visited: pt.visited });
+                } else {
+                    roboMap.get(pt.toString())!.visited++;
+                }
+            } else if (direction === DirectionName.west) {
+                x_robo -= 1;
+                y_robo += 0;
+                const pt = new Point(x_robo, y_robo);
+                if (!roboMap.has(pt.toString())) {
+                    roboMap.set(pt.toString(), { visited: pt.visited });
+                } else {
+                    roboMap.get(pt.toString())!.visited++;
+                }
+            } else if (direction === DirectionName.north) {
+                x_robo -= 0;
+                y_robo += 1;
+                const pt = new Point(x_robo, y_robo);
+                if (!roboMap.has(pt.toString())) {
+                    roboMap.set(pt.toString(), { visited: pt.visited });
+                } else {
+                    roboMap.get(pt.toString())!.visited++;
+                }
+            } else if (direction === DirectionName.south) {
+                x_robo -= 0;
+                y_robo -= 1;
+                const pt = new Point(x_robo, y_robo);
+                if (!roboMap.has(pt.toString())) {
+                    roboMap.set(pt.toString(), { visited: pt.visited });
+                } else {
+                    roboMap.get(pt.toString())!.visited++;
+                }
+            }
+        }
+        i++;
+    }
+
+    const santaEntries = santaMap.entries();
+    const roboEntries = roboMap.entries();
+
+    const visited = new Set<string>();
+
+    for (let i = 0; i < santaMap.size; i++) {
+        const santaEntry = santaEntries.next().value;
+        visited.add(santaEntry[0]);
+    }
+    for (let i = 0; i < roboMap.size; i++) {
+        const roboEntry = roboEntries.next().value;
+        visited.add(roboEntry[0]);
+    }
+
+    answer2 = visited.size;
+    console.log("ANSWER PART 2", answer2);
 })();
