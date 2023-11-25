c5df52a9bd7e693a2d20a9ac674002a99f030a25
dj-viking <anders.swedishviking@gmail.com>
2023-11-25 14:09:43 -0500

    solve part2

--- TypeScript/Day6/index.ts
+++ TypeScript/Day6/index.ts
@@ -4,7 +4,7 @@ const input1 = readFileSync(__dirname + "\\input.txt", { encoding: "utf-8" });
 const instructions1 = input1.split("\r\n");
 const input2 = readFileSync(__dirname + "\\input2.txt", { encoding: "utf-8" });
 const instructions2 = input2.split("\r\n");
-console.log(instructions2);
+// console.log(instructions2);
 
 type Status = boolean;
 
@@ -19,35 +19,51 @@ class LED {
     }
 }
 
+class LEDWithBrightness {
+    public x: number = 0;
+    public y: number = 0;
+    public brightness: number = 0;
+
+    public constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 const LEDGrid: LED[][] = [];
+const LEDGrid2: LEDWithBrightness[][] = [];
 
 // const GRID_SIZE = 11;
 const GRID_SIZE = 1001;
 
 for (let y = 0; y < GRID_SIZE; y++) {
     LEDGrid.push([]); //row
+    LEDGrid2.push([]);
     for (let x = 0; x < GRID_SIZE; x++) {
         // add columns to row
         LEDGrid[y].push(new LED(x, y));
+        LEDGrid2[y].push(new LEDWithBrightness(x, y));
     }
     LEDGrid[y].pop();
+    LEDGrid2[y].pop();
 }
 
 // console.log(LEDGrid);
 
 LEDGrid.pop();
+LEDGrid2.pop();
 
 let answer1 = null;
 let answer2 = null;
 
 type Action = "on" | "off" | "toggle";
 
-function applyInstruction(line: string) {
+function applyInstruction(line: string, part2?: boolean) {
     const action: Action = line
         .split(" ")
         .find((item) => /on|off|toggle/g.test(item)) as Action;
 
-    console.log("what is action => ", action);
+    // console.log("what is action => ", action);
 
     let areainstr: string[] = [];
 
@@ -60,7 +76,7 @@ function applyInstruction(line: string) {
         areainstr = [`${splitLine[1]}`, `${splitLine[2]}`, `${splitLine[3]}`];
     }
 
-    console.log("what is areainstr => ", areainstr);
+    // console.log("what is areainstr => ", areainstr);
 
     const lhArea = areainstr[0];
     const rhArea = areainstr[2];
@@ -73,24 +89,39 @@ function applyInstruction(line: string) {
     const rowRange: number = rowEnd - rowStart;
     const columnRange: number = colEnd - colStart;
 
-    console.log("row range => ", rowRange, "\n", "col range => ", columnRange);
+    // console.log("row range => ", rowRange, "\n", "col range => ", columnRange);
 
     for (let row = rowStart; row <= rowEnd; row++) {
         for (let col = colStart; col <= colEnd; col++) {
             switch (action) {
                 case "on":
                     {
-                        LEDGrid[col][row].status = true;
+                        if (part2) {
+                            LEDGrid2[col][row].brightness++;
+                        } else {
+                            LEDGrid[col][row].status = true;
+                        }
                     }
                     break;
                 case "off":
                     {
-                        LEDGrid[col][row].status = false;
+                        if (part2) {
+                            if (LEDGrid2[col][row].brightness > 0) {
+                                LEDGrid2[col][row].brightness--;
+                            }
+                        } else {
+                            LEDGrid[col][row].status = false;
+                        }
                     }
                     break;
                 case "toggle":
                     {
-                        LEDGrid[col][row].status = !LEDGrid[col][row].status;
+                        if (part2) {
+                            LEDGrid2[col][row].brightness += 2;
+                        } else {
+                            LEDGrid[col][row].status =
+                                !LEDGrid[col][row].status;
+                        }
                     }
                     break;
             }
@@ -119,5 +150,19 @@ function applyInstruction(line: string) {
     console.log("answer1 is", answer1);
 })();
 (function main2() {
+    for (const instr of instructions1) {
+        applyInstruction(instr, true);
+    }
+
+    let totalBrightness = 0;
+
+    for (let i = 0; i < LEDGrid2.length; i++) {
+        for (let j = 0; j < LEDGrid2[0].length; j++) {
+            totalBrightness += LEDGrid2[i][j].brightness;
+        }
+    }
+
+    answer2 = totalBrightness;
+
     console.log("answer2 is", answer2);
 })();
