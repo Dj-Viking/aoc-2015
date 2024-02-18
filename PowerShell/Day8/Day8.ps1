4c60bb61e730f8d769287cadd8a3102893e285ed
dj-viking <anders.swedishviking@gmail.com>
2024-02-18 10:32:20 -0500

    solve part 1 day 8

---
+++ PowerShell/Day8/Day8.ps1
@@ -0,0 +1,97 @@
+param(
+    [Parameter(Mandatory = $true, HelpMessage = "Please enter an input filename")]
+    [System.String]$InputFilename
+)
+
+[String]$answer1 = "answer goes here"
+[String]$answer2 = "answer goes here"
+[String]$myInput = ""
+
+. $PSScriptRoot\..\ReadInput.ps1
+. $PSScriptRoot\..\ParseLines.ps1
+
+$lines = Get-Content -Path "$PSScriptRoot\$InputFilename.txt"
+
+Function Get-CharAmountInMemory {
+    [OutputType([int])]
+    param(
+        [string]$line
+    )
+
+    . {
+        [int]$in_mem_amount = 0;
+        
+        for ($i = 0; $i -lt $line.Length; $i++) {
+            Write-Host "[$($i)] - $([int]([char]$line[$i])) - $($line[$i])" -ForegroundColor Yellow;
+    
+            $asciiCode = [int]([char]$line[$i]);
+            if ($asciiCode -eq 34 -and ($i -eq 0 -or $i -eq ($line.Length - 1))) {
+                continue;
+            }
+            else {
+                # branch if \\
+                if ($asciiCode -eq 92 -and [int]([char]$line[$i + 1]) -eq 92) {
+                    $in_mem_amount += 1;
+                    $i += 1;
+                    continue;
+                }
+                # branch if \ in string and next char is not x
+                if ($asciiCode -eq 92 -and [int]([char]$line[$i + 1]) -ne 120) {
+                    # get next char after this to check if it's a hex escape or another escaped character to add in memory
+                    $in_mem_amount += 1;
+                    $i += 1;
+                    continue;
+                } # branch again if \x(num)(num)
+                elseif ($asciiCode -eq 92 -and [int]([char]$line[$i + 1]) -eq 120) {
+                    $in_mem_amount += 1;
+                    $i += 3;
+                    continue;
+                }
+                $in_mem_amount += 1;
+            }
+        }
+    } | Out-Null;
+
+    return $in_mem_amount;
+
+}
+
+
+Function PartOne {
+    [System.Collections.ArrayList]$charAmountList = [System.Collections.ArrayList]@();
+    [System.Collections.ArrayList]$inMemAmountList = [System.Collections.ArrayList]@();
+    foreach ($line in $lines) {
+        Write-Host "line => $line" -ForegroundColor Green;
+        Write-Host "line length $($line.Length)"
+        $charAmountList.Add($line.Length) | Out-Null;
+        $inMem = Get-CharAmountInMemory -line $line;
+        $inMemAmountList.Add($inMem) | Out-Null;
+        Write-Host "in mem amount => $inMem" -ForegroundColor Cyan;
+        
+    }
+    $charSum = 0;
+    $inMemSum = 0;
+
+    foreach ($num in $charAmountList) {
+        $charSum += $num;
+    }
+    foreach ($num in $inMemAmountList) {
+        $inMemSum += $num;
+    }
+
+    Write-Host "char sum $charSum - inmem sum $inMemSum" -ForegroundColor Magenta;
+
+    $answer1 = $charSum - $inMemSum;
+
+    # 1448 is TOO HIGH!!!!!!
+    
+    Write-Host "[INFO]: solving part one..." -ForegroundColor Cyan
+    Write-Host "[INFO]: part one answer is $answer1" -ForegroundColor Green
+}
+Function PartTwo {
+    Write-Host "[INFO]: solving part two..." -ForegroundColor Cyan
+    Write-Host "[INFO]: part two answer is $answer2" -ForegroundColor Green
+}
+
+PartOne
+# PartTwo
