4dfd5954972157f94461b9d0343bfdede2baf1ce
dj-viking <anders.swedishviking@gmail.com>
2024-11-25 01:15:45 -0500

    solve part 2 day3 in RUSTsss hell fucking yeah

--- rust/day3/src/main.rs
+++ rust/day3/src/main.rs
@@ -78,37 +78,88 @@ pub struct Visited {
 }
 
 fn main() {
-    let mut point = Point::new(0, 0);
+    let mut santa = Point::new(0, 0);
+    let mut robo = Point::new(0, 0);
 
-    let mut visited = HashMap::<Visited, i64>::new();
+    let mut visited_santa_one = HashMap::<Visited, i64>::new();
 
     // start at 0,0
-    visited.entry(Visited { coords: point.to_string() }).or_insert(1);
+    visited_santa_one.entry(Visited { coords: santa.to_string() }).or_insert(1);
 
     std::fs::read_to_string("input")
         .unwrap()
         .chars()
         .for_each(|l| match l {
             '^' => {
-                point.go_up(&mut visited);
+                santa.go_up(&mut visited_santa_one);
             },
             'v' => {
-                point.go_down(&mut visited);
+                santa.go_down(&mut visited_santa_one);
             },
             '>' => {
-                point.go_right(&mut visited);
+                santa.go_right(&mut visited_santa_one);
             },
             '<' => {
-                point.go_left(&mut visited);
+                santa.go_left(&mut visited_santa_one);
             },
             _ => (), 
         });
     let mut at_least_once = 0;
-    for (_k, v) in &visited {
+    for (_k, v) in &visited_santa_one {
         //println!("{k:?} -> {v}");
         if *v >= 1 {
             at_least_once += 1;
         }
     } 
     println!("part 1: {}", at_least_once);
+
+//////////////////////////////////////////////////////
+
+    let mut santa = Point::new(0, 0);
+    let mut robo = Point::new(0, 0);
+    
+    let mut visited = HashMap::<Visited, i64>::new();
+
+    visited.entry(Visited { coords: santa.to_string() })
+        .or_insert(1);
+    visited.entry(Visited { coords: robo.to_string() })
+        .and_modify(|e| *e += 1);
+
+    let mut counter = 0;
+    let dirs = std::fs::read_to_string("input")
+        .unwrap()
+        .chars()
+        .for_each(|c| {
+
+            if counter % 2 == 0 {
+                match c {
+                    '^' => santa.go_up(&mut visited),
+                    '>' => santa.go_right(&mut visited),
+                    '<' => santa.go_left(&mut visited),
+                    'v' => santa.go_down(&mut visited),
+                    _ => (),
+                }
+            } else {
+                match c {
+                    '^' => robo.go_up(&mut visited),
+                    '>' => robo.go_right(&mut visited),
+                    '<' => robo.go_left(&mut visited),
+                    'v' => robo.go_down(&mut visited),
+                    _ => (),
+                }
+            }            
+
+            counter += 1;
+        });
+
+
+    let mut at_least_once = 0;
+    
+    for (k, v) in &visited {
+        if *v >= 1 {
+            at_least_once += 1;
+        }
+    } 
+    println!("part 2: {}", at_least_once);
+
 }
