6896531cd7851b082a88904034f59712244032c1
dj-viking <anders.swedishviking@gmail.com>
2024-11-25 00:17:37 -0500

    clean up

--- rust/day3/src/main.rs
+++ rust/day3/src/main.rs
@@ -13,17 +13,57 @@ impl Point {
             y: cy,
         }
     }
-    pub fn go_up(&mut self) -> () {
+    pub fn go_up(&mut self, visited: &mut HashMap::<Visited, i64>) -> () {
         self.y += 1;
+        if !visited.contains_key(&Visited { coords: self.to_string() }) {
+            visited.entry(
+                Visited { coords: self.to_string() }
+            ).or_insert(1);
+
+        } else { 
+            visited.entry(
+                Visited { coords: self.to_string() }
+            ).and_modify(|c| *c += 1); 
+        }
     }
-    pub fn go_down(&mut self) -> () {
+    pub fn go_down(&mut self, visited: &mut HashMap::<Visited, i64>) -> () {
         self.y -= 1;
+        if !visited.contains_key(&Visited { coords: self.to_string() }) {
+            visited.entry(
+                Visited { coords: self.to_string() }
+            ).or_insert(1);
+
+        } else { 
+            visited.entry(
+                Visited { coords: self.to_string() }
+            ).and_modify(|c| *c += 1); 
+        }
     }
-    pub fn go_left(&mut self) -> () {
+    pub fn go_left(&mut self, visited: &mut HashMap::<Visited, i64>) -> () {
         self.x -= 1;
+        if !visited.contains_key(&Visited { coords: self.to_string() }) {
+            visited.entry(
+                Visited { coords: self.to_string() }
+            ).or_insert(1);
+
+        } else { 
+            visited.entry(
+                Visited { coords: self.to_string() }
+            ).and_modify(|c| *c += 1); 
+        }
     }
-    pub fn go_right(&mut self) -> () {
+    pub fn go_right(&mut self, visited: &mut HashMap::<Visited, i64>) -> () {
         self.x += 1;
+        if !visited.contains_key(&Visited { coords: self.to_string() }) {
+            visited.entry(
+                Visited { coords: self.to_string() }
+            ).or_insert(1);
+
+        } else { 
+            visited.entry(
+                Visited { coords: self.to_string() }
+            ).and_modify(|c| *c += 1); 
+        }
     }
 
     pub fn to_string(&self) -> String {
@@ -32,8 +72,8 @@ impl Point {
 
 }
 
-#[derive(Hash, Eq, PartialEq, Debug)]
-struct Visited {
+#[derive(Hash, Eq, PartialEq, Debug, Clone)]
+pub struct Visited {
     coords: String,
 }
 
@@ -41,79 +81,34 @@ fn main() {
     let mut point = Point::new(0, 0);
 
     let mut visited = HashMap::<Visited, i64>::new();
+
+    // start at 0,0
     visited.entry(Visited { coords: point.to_string() }).or_insert(1);
-
-    // println!("visited {:?}", visited);
-    // for (point_coords, freq) in &visited {
-    //     println!("{point_coords:?} been there -> {freq}");
-    // }
-
-    println!("start {:?}", point.to_string());
 
     std::fs::read_to_string("input")
         .unwrap()
         .chars()
         .for_each(|l| match l {
             '^' => {
-                point.go_up();
-                if !visited.contains_key(&Visited { coords: point.to_string() }) {
-                    visited.entry(
-                        Visited { coords: point.to_string() }
-                    ).or_insert(1);
-
-                } else { 
-                    visited.entry(
-                        Visited { coords: point.to_string() }
-                    ).and_modify(|c| *c += 1); 
-                }
+                point.go_up(&mut visited);
             },
             'v' => {
-                point.go_down();
-                if !visited.contains_key(&Visited { coords: point.to_string() }) {
-                    visited.entry(
-                        Visited { coords: point.to_string() }
-                    ).or_insert(1);
-
-                } else { 
-                    visited.entry(
-                        Visited { coords: point.to_string() }
-                    ).and_modify(|c| *c += 1); 
-                }
+                point.go_down(&mut visited);
             },
             '>' => {
-                point.go_right();
-                if !visited.contains_key(&Visited { coords: point.to_string() }) {
-                    visited.entry(
-                        Visited { coords: point.to_string() }
-                    ).or_insert(1);
-
-                } else { 
-                    visited.entry(
-                        Visited { coords: point.to_string() }
-                    ).and_modify(|c| *c += 1); 
-                }
+                point.go_right(&mut visited);
             },
             '<' => {
-                point.go_left();
-                if !visited.contains_key(&Visited { coords: point.to_string() }) {
-                    visited.entry(
-                        Visited { coords: point.to_string() }
-                    ).or_insert(1);
-
-                } else { 
-                    visited.entry(
-                        Visited { coords: point.to_string() }
-                    ).and_modify(|c| *c += 1); 
-                }
+                point.go_left(&mut visited);
             },
             _ => (), 
         });
     let mut at_least_once = 0;
-    for (k, v) in &visited {
-        println!("{k:?} -> {v}");
+    for (_k, v) in &visited {
+        //println!("{k:?} -> {v}");
         if *v >= 1 {
             at_least_once += 1;
         }
     } 
-    println!("something {}", at_least_once);
+    println!("part 1: {}", at_least_once);
 }
